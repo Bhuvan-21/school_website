7e21a95725e72b5cfa26ce7196e7ea70db61203a
Bhuvan Sachdeva <sachdeva.bhuvan21@gmail.com>
2020-07-01 22:49:29 +0530

    ..

--- school_web/hackin/interface/models.py
+++ school_web/hackin/interface/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    adm_no = models.IntegerField()
+    first_name = models.TextField(max_length = 20)
+    last_name = models.TextField(max_length = 20)
+    subject = models.TextField(max_lentgh = 20)
+    working_days = models.IntegerField()
+    present_days = models.IntegerField()
+    half_days = models.IntegerField()
+    absent_days = models.IntegerField()
+    
