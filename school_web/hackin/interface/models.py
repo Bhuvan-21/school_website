6cade919bc1637f2dce40c399a68b6c48fc071c0
Bhuvan Sachdeva <sachdeva.bhuvan21@gmail.com>
2020-07-01 23:13:53 +0530

    ..

--- school_web/hackin/interface/models.py
+++ school_web/hackin/interface/models.py
@@ -5,9 +5,70 @@ class Student(models.Model):
     adm_no = models.IntegerField()
     first_name = models.TextField(max_length = 20)
     last_name = models.TextField(max_length = 20)
-    subject = models.TextField(max_lentgh = 20)
     working_days = models.IntegerField()
     present_days = models.IntegerField()
     half_days = models.IntegerField()
     absent_days = models.IntegerField()
+    marks_eng_mt = models.IntegerField()
+    marks_eng_ft = models.IntegerField()
+    marks_math_mt = models.IntegerField()
+    marks_math_ft = models.IntegerField()
+    marks_sci_mt = models.IntegerField()
+    marks_sci_ft = models.IntegerField()
+    marks_sst_mt = models.IntegerField()
+    marks_sst_ft = models.IntegerField()
+    marks_hin_mt = models.IntegerField()
+    marks_hin_ft = models.IntegerField()
+
+    def get_adm_no(self):
+        return self.adm_no
+
+    def get_first_name(self):
+        return self.first_name 
+
+    def get_last_name(self):
+        return self.last_name
+
+    def get_working_days(self):
+        return self.working_days
+
+    def get_present_days(self):
+        return self.present_days
+    
+    def get_half_days(self):
+        return self.half_days
+
+    def get_absent_days(self):
+        return self.absent_days
+
+    def get_marks_eng_mt(self):
+        return self.marks_eng_mt
+
+    def get_marks_eng_ft(self):
+        return self.marks_eng_ft
+
+    def get_marks_math_mt(self):
+        return self.marks_math_mt
+
+    def get_marks_math_ft(self):
+        return self.marks_math_ft
+
+    def get_marks_hin_mt(self):
+        return self.marks_hin_mt
+
+    def get_marks_hin_ft(self):
+        return self.marks_hin_ft
+
+    def get_marks_sci_mt(self):
+        return self.marks_sci_mt
+
+    def get_marks_sci_ft(self):
+        return self.marks_sci_ft
+
+    def get_marks_sst_mt(self):
+        return self.marks_sst_mt
+
+    def get_marks_sst_ft(self):
+        return self.marks_sst_ft
+
     
